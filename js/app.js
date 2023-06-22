6cfcd5af98a8b068610aa63ea63524411d0b5513
usigna <sylwia.fait@gmail.com>
2023-06-22 18:49:08 +0200

    Add base files

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// for production only
+// window.onerror = function(message, url, lineNumber) {  
+//   return true;
+// };
+
+const init = function () {
+
+};
+
+document.addEventListener('DOMContentLoaded', init);
